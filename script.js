512890ed36d9c60db1918eafd76db8badbd3e0e0
Hasuru <hugo.as.silva25@gmail.com>
2022-10-14 11:00:22 +0100

    Layoutzinho

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("logNav").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+}
+
+function closeNav() {
+    document.getElementById("logNav").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
+}
