f0b4df865fe46bcc6bffb7db51eb27954266312e
4samoT <tomas.ferreira.02@gmail.com>
2022-10-23 06:06:10 +0100

    A um erro de ser terminado

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function closeRules() {
     document.getElementById('ruleNav').style.display = 'none';
 }
 
-var w, difficulty = 4, el = new Array(), game, execnt=0, canclk = new Array(); 
+var w, difficulty = 4, el = new Array(), game, execnt=0, maxcols, canclk = new Array(); 
 
 window.addEventListener('load',() => {
     game = document.getElementsByClassName("board");
@@ -25,86 +25,79 @@ window.addEventListener('load',() => {
     num.addEventListener('input', function () {
         let val = num.valueAsNumber;
         w = val;
-        console.log(typeof val, val);
+        //console.log(typeof val, val);
     });
 
-    el[0] = 0;
+    el[0] = 0; canclk[0] = 0;
+    if(w>=4) maxcols=1;
+        else maxcols=5;
 
     for(let i=1; i<=w; i++){
-        el[i]=w+1-i;
+        el[i]=maxcols+i*2-2;
         canclk[i]=1;
     }
 
     if(execnt==0){
-        create_game();
+        var msz=2*w-1;
+        for(let k=1; k<=w; k++){
+            let row = document.createElement("div");
+            row.className = "inner_game_piece";
+            for(let l=1; l<=maxcols; l++){
+                let elem = document.createElement("div");
+                elem.className = "game_piece";
+                elem.onclick = function(){
+                    if(canclk[k]==1){
+                        player_move(k);
+                        row.removeChild(row.lastElementChild);
+                        if(winCheck()) checkmate("P");
+                    }
+                }
+                row.appendChild(elem);
+            }
+            game[0].appendChild(row);
+            maxcols+=2;
+        }
         execnt++;
     }
-
-    console.log(canclk[1], el[2]);
 });
 
-function create_game(){
-    var maxcols=1, wr=9;
-    //9 as a limit for a better gameplay
-    for(let i=1; i<wr; i++){
-        let col = document.createElement("div");
-        col.className = "inner_game_piece";
-        for(let j=1; j<=maxcols; j++){
-            let elem = document.createElement("div");
-            elem.className = "game_piece";
-            elem.onclick = function(){player_move(j,elem);}
-            col.appendChild(elem);
-        }
-        game[0].appendChild(col);
-        if(maxcols<w) maxcols++;
-    }
-}
-
-function disenabler(col){
+function disenabler(pile){
     for(let i=1; i<=w; i++){
+        if(pile==-1) canclk[i]=-1;
         if(canclk[i]==2) canclk[i]--;
-        if(i!=col && canclk[i]!=0 && col!=0) canclk[i]=2;
+        if(i!=pile && canclk[i]!=0 && pile!=0) canclk[i]=2;
     }
 }
 
-function player_move(col, elem) {
-    if(canclk[col]==1){
-        el[col]--;
-        disenabler(col);
-        gb_update(elem);
-        var activelems = 0; 
-        for(let i=1; i<=w; i++){activelems += i }
-        if(activelems==0){
-            checkmate("P");
-        }
-        else{
-            ai_move();
-        }
+function player_move(pile) {
+    if(canclk[pile]==1){
+        el[pile]--;
+        if(el[pile]==0) canclk[pile] = 0;
+        disenabler(pile);
+        if(winCheck()) checkmate("P");
     }
 }
 
-function diff_select(){
-    var subm;
-    subm = document.getElementsByName('diff');
+function endOfTurn(){
+    disenabler(-1);
+    ai_move();
+    disenabler(0);
+}
 
-    for(let i=0; i<4; i++){
-        if(subm[i] = checked){
-            diff = subm[i].value;
-            break;
-        }
-    }
-
-    console.log(difficulty);
+function winCheck(){
+    var activelems=0;
+    for(let i=1; i<=w; i++){activelems += el[i] }
+    console.log(activelems);
+    if(activelems==0) return true;
+    else return false;
 }
 
 function ai_move(){
-    var activelems = 0; 
-    for(let i=1; i<=w; i++){activelems += i }
-    if(activelems==0){
-        checkmate("P");
-    }
-    var chance;
-    //to finish
+    if(winCheck()) checkmate("P");
+    var chance = document.querySelector('input[name="diff"]:checked').value;
+    var rn = Math.floor(Math.random() * 10) + 1;
+    if(rn>chance) random_play();
+    else winner_move();
 }
 
 function isWinning(){
@@ -122,11 +115,24 @@ function isWinning(){
 }
 
 function random_play(){
-    var ran = 0;
-    while(el[ran]==0){
-        ran = Math.floor(Math.random() * w) + 1;
+    var pile = 0, qnt=0;
+    while(el[pile]==0){
+        pile = Math.floor(Math.random() * w) + 1;
     }
-    return Math.floor(Math.random() * el[ran]) + 1;
+    qnt = Math.floor(Math.random() * el[ran]) + 1;
+
+    var gm = game[0].getElementsByTagName("div");
+    for(let i=1; i<gm.length; i++){
+        if(i==pile){
+            var rw = gm[i].getElementsByTagName("div");
+            for(let j=1; j<=qnt; j++){
+                el[pile]--;
+                rw.removeChild(rw.lastElementChild);
+            }
+            break;
+        }
+    }
+    if(winCheck()) checkmate("C");
 }
 
 function dec2bin(dec) {
@@ -134,16 +140,44 @@ function dec2bin(dec) {
   }
 
 function winner_move(){
-    var pair = int(w+1);
-    var pieces;
+    var pile=2, quantity=2, flag=0, counter=0;
+    var pair = new Array();
+    var pieces = new Array();
     for(let i=1; i<=w; i++){
         if(el[i]==0) continue;
         pieces = dec2bin(el[i]);
         for(let j=0; j<pieces.length; j++){
-            pair[j] += pieces[j];
+            var v = pieces.charCodeAt(j)-48; var op=0;
+            if(v==1){
+                if(pair[j]%2==0){
+                    op = 1;
+                    flag = i;
+                    counter += Math.pow(2,j);
+                } else{
+                    op = -1;
+                    flag = 0;
+                    counter -= Math.pow(2,j);
+                }
+            }
+            pair[j] += op*v;
         }
-        //have to finish, lacks knowing which line and how many elems to take out
     }
+
+    pile=flag; quantity=counter;
+    if(quantity==0) random_play(); 
+
+    var gm = game[0].getElementsByTagName("div");
+    for(let i=1; i<gm.length; i++){
+        if(i==pile){
+            var rw = gm[i].getElementsByTagName("div");
+            for(let j=1; j<=quantity; j++){
+                el[pile]--;
+                rw.removeChild(rw.lastElementChild);
+            }
+            break;
+        }
+    }
+    if(winCheck()) checkmate("C");
 }
 
 function checkmate(winner){
@@ -159,6 +193,7 @@ function checkmate(winner){
 
 function gb_update(elem){
     //how to manipulate a certain element for it to turn on another color and, if possible, taking out its clicking ability?
+    console.log("here!");
     elem.style.backgroundColor = "rgb(131, 128, 135)";
     elem.onclick = null;
 }
